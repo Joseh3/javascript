e0016587094bf89149b0a2d740be08f3de385ec9
Jose Huamani <sv44834543@idat.edu.pe>
2023-05-03 22:11:14 -0500

    actualizando

---
+++ app.js
@@ -0,0 +1,37 @@
+//control de fujo 
+// if / else
+
+//const age=10;
+//const card=true;
+const mes=10;
+const dia=15;
+
+//condición simple 
+if ((dia>=21 && mes==3 ) || (dia<=20 && mes==4 )) {
+    console.log('Su signo zodiacal es Aries');
+}else if  ((dia>=24 && mes==9 ) || (dia<=23 && mes==10 )) {
+    console.log('Su signo zodiacal es Libra');    
+}else if  ((dia>=21 && mes==4 ) || (dia<=21 && mes==5 )) {
+    console.log('Su signo zodiacal es Tauro');    
+}else if  ((dia>=24 && mes==10 ) || (dia<=22 && mes==11 )) {
+    console.log('Su signo zodiacal es Escorpio');    
+}else if  ((dia>=22 && mes==5 ) || (dia<=21 && mes==6 )) {
+    console.log('Su signo zodiacal es Geminis');    
+}else if  ((dia>=23 && mes==11 ) || (dia<=21 && mes==12 )) {
+    console.log('Su signo zodiacal es Sagitario');    
+}else if  ((dia>=21 && mes==6 ) || (dia<=23 && mes==7 )) {
+    console.log('Su signo zodiacal es Cancer');           
+}else if  ((dia>=22 && mes==12 ) || (dia<=20 && mes==1 )) {
+    console.log('Su signo zodiacal es Capricornio');    
+}else if  ((dia>=24 && mes==7 ) || (dia<=23 && mes==8 )) {
+    console.log('Su signo zodiacal es Leo');    
+}else if  ((dia>=21 && mes==1 ) || (dia<=19 && mes==2 )) {
+    console.log('Su signo zodiacal es Acuario');    
+}else if  ((dia>=24 && mes==8 ) || (dia<=23 && mes==9 )) {
+    console.log('Su signo zodiacal es Virgo');    
+}else if  ((dia>=20 && mes==2 ) || (dia<=20 && mes==3 )) {
+    console.log('Su signo zodiacal es Piscis');                                                                                 
+}
+
+
+
